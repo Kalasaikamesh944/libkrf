7af11b810758bb28aac50d33d077bc61a5a4bc03
Kala@185 <saikamesh.y@gmail.com>
2025-03-18 17:31:53 +0530

    update

---
+++ build/CMakeFiles/basic_capture.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_capture.dir/examples/basic_capture.cpp.o"
+  "CMakeFiles/basic_capture.dir/examples/basic_capture.cpp.o.d"
+  "basic_capture"
+  "basic_capture.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
